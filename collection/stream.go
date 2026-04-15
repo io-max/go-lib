fc33c95e480e77daaf4a0e33fb489f0267e669a9
Max <io.better.chen@gmail.com>
2026-04-15 17:33:34 +0800

    refactor(collection): restructure Stream for lazy evaluation

--- collection/stream.go
+++ collection/stream.go
@@ -1,26 +1,42 @@
 package collection
 
 type Stream[T any] struct {
-	data []T
+	executor func() []T
+	data     []T
 }
 
 func Of[T any](collection []T) *Stream[T] {
 	if collection == nil {
 		collection = []T{}
 	}
-	return &Stream[T]{data: collection}
+	return &Stream[T]{
+		data: collection,
+		executor: func() []T {
+			return collection
+		},
+	}
 }
 
 func (s *Stream[T]) Collect() []T {
+	if s.executor != nil {
+		s.data = s.executor()
+		s.executor = nil
+	}
 	result := make([]T, len(s.data))
 	copy(result, s.data)
 	return result
 }
 
 func Map[T, U any](s *Stream[T], fn func(T) U) *Stream[U] {
-	result := make([]U, len(s.data))
-	for i, item := range s.data {
-		result[i] = fn(item)
+	executor := func() []U {
+		data := s.Collect()
+		result := make([]U, len(data))
+		for i, item := range data {
+			result[i] = fn(item)
+		}
+		return result
 	}
-	return &Stream[U]{data: result}
+	return &Stream[U]{
+		executor: executor,
+	}
 }
